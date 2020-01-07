2429c546bd66f04947ae8dff4749062d35e94d23
tommy3wood <thomaswcurran1014@gmail.com>
2020-01-07 11:53:40 -0600

    add employee.rb and store_item.rb to git

---
+++ object_oriented_ruby/store_item.rb
@@ -0,0 +1,53 @@
+# car_1 = {make: "buick", model: "riviara", color: "red", price: 3000, year: 1991, stock: true}
+# car_2 = {:make => "Tesla", :model => "space truck thing", :color => "blue", :price => 67000, :year => 2020, :stock => false}
+# car_3 = {:make => "Toyota", :model => "prius", :color => "grey", :price => 25000, :year => 2015, :stock => true}
+
+# puts "In #{ car_1[:year] } #{ car_1[:make] } created the #{ car_1[:model] }. Today it can be had at police auctions for a modest #{ car_1[:price]}"
+# puts "The hottest #{ car_2[:make] } on the markey is the #{ car_2[:model] }, I guess. It's going to set you back #{ car_2[:price]}"
+# puts "An economic and green option is the classic #{ car_3[:model]}. A used #{ car_3[:year]} model will probably set you back #{ car_3[:price]}."
+
+class Car
+
+  def initialize(make, model, color, price, year, stock)
+    @make = make
+    @model = model
+    @color = color
+    @price = price 
+    @year = year
+    @stock = stock
+  end
+
+  def make
+    @make
+  end
+
+  def model
+    @model
+  end
+
+  def year
+    @year
+  end
+
+  def price
+    @price
+  end
+
+  def price=(new_price)
+    @price = new_price
+  end
+
+  def print_info
+    p "this #{make} #{model} was manufactured in #{year} and CBB says it runs about #{price}"
+    
+  end
+
+end
+
+car_1 = Car.new("buick", "riviara", "red", 3000, 1991, true)
+car_2 = Car.new("Tesla", "space truck thing", "blue", 67000, 2020, false)
+car_3 = Car.new("Toyota", "prius", "grey", 25000, 2015, true)
+
+car_1.print_info
+p car_2.model
+p car_3.price
---
+++ staff/employee.rb
@@ -0,0 +1,71 @@
+# employee_1 = ["Dave", "Thomas", 79000, false]
+# employee_2 = ["Han", "Solo", 24000, true]
+
+# puts employee_1[0] + " " + employee_1[1] + " makes " + employee_1[2].to_s + " a year."
+# puts "#{ employee_2[0] } #{employee_2[1] } makes #{ employee_2[2] } a year."
+
+# employee_1 = {first_name: "Dave", last_name: "Thomas", salary: 79000, active: false}
+# employee_2 = {:first_name => "Han", :last_name => "Solor", :salary => 24000, :active => true}
+
+# puts "#{ employee_1[:first_name] } #{ employee_1[:last_name] } makes #{ employee_1[:salary] } a year"
+# puts "#{ employee_2[:first_name] } #{ employee_2[:last_name] } makes #{ employee_2[:salary] } a year"
+
+class Employee
+
+  def initialize(input_first_name, input_last_name, input_salary, input_active)
+    @first_name = input_first_name
+    @last_name = input_last_name
+    @salary = input_salary
+    @active = input_active
+  end
+
+  def first_name
+    @first_name
+  end
+
+  def last_name
+    @last_name
+  end
+
+  def salary
+    @salary
+  end
+
+  def active
+    @active
+  end
+
+  def first_name=(new_first_name)
+    @first_name = new_first_name
+  end
+
+  def print_info
+    puts "#{ first_name } #{ last_name } makes #{ salary } per year."
+  end 
+
+  def give_annual_raise
+    @salary = @salary * 1.05
+  end
+
+  def a
+    "Jim"
+  end
+
+  def b
+    a # calling b will now return "Jim"
+  end
+
+end
+
+employee_1 = Employee.new("Dave", "Thomas", 79000, false)
+employee_2 = Employee.new("Han", "Solo", 24000, true)
+
+employee_1.print_info
+employee_2.print_info
+employee_1.give_annual_raise
+employee_2.give_annual_raise
+employee_1.print_info
+employee_2.print_info
+p employee_1.first_name
+employee_1.first_name = "Bob"
+p employee_1.first_name
