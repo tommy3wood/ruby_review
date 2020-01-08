7fa548e773f293fcf2011334b28ad2be05d4948f
tommy3wood <thomaswcurran1014@gmail.com>
2020-01-08 09:36:47 -0600

    Refactoring arguments in store_item.rb  to be hashes

--- object_oriented_ruby/store_item.rb
+++ object_oriented_ruby/store_item.rb
@@ -10,13 +10,17 @@ class Car
   attr_reader :make, :model, :year, :price
   attr_writer :price
 
-  def initialize(make, model, color, price, year, stock)
-    @make = make
-    @model = model
-    @color = color
-    @price = price 
-    @year = year
-    @stock = stock
+  def initialize(input_options)
+    @make = input_options[:first_name]
+    @model = input_options[:model]
+    @color = input_options[:color]
+    @price = input_options[:price]
+    @year = input_options[:year]
+    @stock = input_options[:stock]
+  end
+
+  def opposite_day_sale
+    @price = @price * 1.2
   end
 
   def price=(new_price)
@@ -25,21 +29,44 @@ class Car
 
   def print_info
     p "this #{make} #{model} was manufactured in #{year} and CBB says it runs about #{price}"
-    
   end
 
 end
 
-car_1 = Car.new("buick", "riviara", "red", 3000, 1991, true)
-car_2 = Car.new("Tesla", "space truck thing", "blue", 67000, 2020, false)
-car_3 = Car.new("Toyota", "prius", "grey", 25000, 2015, true)
+car_1 = Car.new(
+                make: "buick", 
+                model: "riviara", 
+                color: "red", 
+                price: 3000, 
+                year: 1991, 
+                stock: true
+                )
+car_2 = Car.new(
+                first_name: "Tesla", 
+                model: "space truck thing", 
+                color: "blue", 
+                price: 67000, 
+                year: 2020, 
+                stock: false
+                )
+car_3 = Car.new(
+                make: "Toyota", 
+                model: "prius", 
+                color: "grey", 
+                price: 25000, 
+                year: 2015, 
+                stock: true
+                )
 
 car_1.print_info
 p car_2.model
-p car_3.price
-p car_3
-puts "=" * 40
-p "PRIUS SALE"
-puts "=" * 40
+
+puts "=" * 30
+p car_2
+car_2.opposite_day_sale
+p car_2
+puts "=" * 30
+p "PRIUS FLASH SALE"
+puts "=" * 30
 car_3.price = 15000
 p car_3
--- staff/employee.rb
+++ staff/employee.rb
@@ -11,32 +11,14 @@
 # puts "#{ employee_2[:first_name] } #{ employee_2[:last_name] } makes #{ employee_2[:salary] } a year"
 
 class Employee
+  attr_reader :first_name, :last_name, :salary, :active
+  attr_writer :first_name, :last_name, :active
 
   def initialize(input_first_name, input_last_name, input_salary, input_active)
     @first_name = input_first_name
     @last_name = input_last_name
     @salary = input_salary
     @active = input_active
-  end
-
-  def first_name
-    @first_name
-  end
-
-  def last_name
-    @last_name
-  end
-
-  def salary
-    @salary
-  end
-
-  def active
-    @active
-  end
-
-  def first_name=(new_first_name)
-    @first_name = new_first_name
   end
 
   def print_info
@@ -47,14 +29,6 @@ class Employee
     @salary = @salary * 1.05
   end
 
-  def a
-    "Jim"
-  end
-
-  def b
-    a # calling b will now return "Jim"
-  end
-
 end
 
 employee_1 = Employee.new("Dave", "Thomas", 79000, false)
@@ -63,9 +37,8 @@ employee_2 = Employee.new("Han", "Solo", 24000, true)
 employee_1.print_info
 employee_2.print_info
 employee_1.give_annual_raise
-employee_2.give_annual_raise
-employee_1.print_info
 employee_2.print_info
+
 p employee_1.first_name
 employee_1.first_name = "Bob"
 p employee_1.first_name
